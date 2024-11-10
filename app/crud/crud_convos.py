cf516d383bac28549a343331288e4c06f6051662
vladimirsgit <stratulatvladimir@protonmail.com>
2024-11-10 13:54:47 +0200

    finalize

--- app/crud/crud_convos.py
+++ app/crud/crud_convos.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models.conversation import Conversation
 from sqlmodel import select
-
+from sqlalchemy import desc
 async def add_convo(db_session: AsyncSession, conversation: Conversation):
     db_session.add(conversation)
     await db_session.commit()
@@ -12,6 +12,7 @@ async def add_convo(db_session: AsyncSession, conversation: Conversation):
 
 async def read_convos(db_session: AsyncSession):
     query = select(Conversation)
+    query = query.order_by(desc(Conversation.created_at))
     res = await db_session.execute(query)
     
     return res.scalars().all()
